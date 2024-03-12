cd417534f0229a8951cf072eece4b1fb9c0e2424
Uziel <201274@ids.upchiapas.edu.mx>
2024-03-11 19:53:09 -0700

    socket

--- app/components/Socket.js
+++ app/components/Socket.js
@@ -2,5 +2,5 @@
 import {io} from 'socket.io-client';
 import configServer from '../../configServer';
 
-const socket = io(configServer.socket.host);
+const socket = io(configServer.api.host);
 export default socket;
--- configServer.js
+++ configServer.js
@@ -5,10 +5,6 @@ module.exports = {
         host: 'http://52.91.230.135'
     },
 
-    socket: {
-        host: 'ws://52.91.230.135'
-    },
-
 
     secretKey : '1223334444555556'
 }
